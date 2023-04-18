47fb9858f5fea91729a6d29de67beb48307bd6c5
Mehdi Saber <m1d3s@protonmail.com>
2023-04-18 14:33:40 +0200

    🚧 Compression, CORS & WIP Rate Limiting

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as compression from 'compression';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.use(compression());
   await app.listen(3000);
 }
 bootstrap();
